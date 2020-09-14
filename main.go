a49d73bb58d618f56c867dcace39b92258a425de
Ciprian Dorin Craciun <ciprian@volution.ro>
2020-09-14 16:35:30 +0300

    [profiling]  Add code snippets to profile CPU and memory (currently disabled).

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"runtime"
+	"runtime/pprof"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -8,10 +11,27 @@ import (
 )
 
 func main() {
+	
+	if false {
+		cpuProfile := "/tmp/netlifyctl-cpu.prof"
+		cpuFile, err := os.Create(cpuProfile) ; if err != nil { panic (err) }
+		defer cpuFile.Close ()
+		if err := pprof.StartCPUProfile (cpuFile); err != nil { panic (err) }
+		defer pprof.StopCPUProfile ()
+	}
+	
 	logrus.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp:    true,
 		DisableTimestamp: false,
 		TimestampFormat:  time.RFC822Z,
 	})
 	commands.Execute()
+	
+	if false {
+		memProfile := "/tmp/netlifyctl-mem.prof"
+		memFile, err := os.Create(memProfile) ; if err != nil { panic (err) }
+		defer memFile.Close ()
+		runtime.GC ()
+		if err := pprof.WriteHeapProfile (memFile); err != nil { panic (err) }
+	}
 }
